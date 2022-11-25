e67acf82efa53d42c63cac87ecf600e99ce4cc7c
mirland <mirland@xmartlabs.com>
2022-11-25 09:50:45 -0300

    Environments improvements

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -9,23 +9,8 @@
 
 import 'package:flutter/widgets.dart';
 
-class $AssetsEnvironmentsGen {
-  const $AssetsEnvironmentsGen();
-
-  /// File path: assets/environments/.env
-  String get env => 'assets/environments/.env';
-
-  /// File path: assets/environments/development.env
-  String get development => 'assets/environments/development.env';
-
-  /// List of all assets
-  List<String> get values => [env, development];
-}
-
 class Assets {
   Assets._();
-
-  static const $AssetsEnvironmentsGen environments = $AssetsEnvironmentsGen();
 }
 
 class AssetGenImage {
