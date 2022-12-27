49e70e84a1bd9775f766a8ec28a878df4dc99342
Federico Correa <federico.correa.lima@gmail.com>
2022-12-27 15:58:44 -0300

    fix bricks path

--- bricks/xl_cubit_screen/__brick__/{{name}}/{{name.snakeCase()}}_screen.dart
+++ bricks/xl_cubit_screen/__brick__/{{name}}/{{name.snakeCase()}}_screen.dart
@@ -4,7 +4,7 @@ class {{name.pascalCase()}}Screen extends StatelessWidget {
   {{name.pascalCase()}}Screen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  Widget build(BuildContext context) =>
+     Container();
+  
 }
