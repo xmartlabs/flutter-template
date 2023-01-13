791291eeb7e75ea298e89515cbdbcb3f860efe86
Nicolás Lantean <nlantean@hotmail.com>
2023-01-13 09:47:26 -0300

    feat: initialization process improvements

--- lib/core/di/di_utils_module.dart
+++ lib/core/di/di_utils_module.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_template/core/source/common/app_database.dart';
 import 'package:flutter_template/ui/app_router.dart';
 import 'package:get_it/get_it.dart';
 
@@ -11,10 +12,21 @@ class UtilsDiModule {
   void setupModule(GetIt locator) {
     locator._setupModule();
   }
+
+  Future<void> setupProviders(GetIt locator) async {
+    locator._setupProviders();
+    await locator.allReady();
+  }
 }
 
 extension _GetItUseCaseDiModuleExtensions on GetIt {
   void _setupModule() {
     registerSingleton(AppRouter());
+  }
+
+  void _setupProviders() {
+    registerSingletonAsync(
+      () => $FloorAppDatabase.databaseBuilder('app_database.db').build(),
+    );
   }
 }
