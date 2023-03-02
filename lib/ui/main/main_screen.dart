c19d2f9c156698b750ca0b7ab72e604ec5277a26
Matías Irland <mirland@xmartlabs.com>
2023-03-02 16:16:04 -0300

    feat: Use guards to handle the authorization state (#142)

--- lib/ui/main/main_screen.dart
+++ lib/ui/main/main_screen.dart
@@ -1,12 +1,8 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_template/core/di/di_provider.dart';
-import 'package:flutter_template/core/model/authentication_status.dart';
-import 'package:flutter_template/ui/app_router.dart';
-import 'package:flutter_template/ui/main/main_cubit.dart';
+import 'package:flutter_template/ui/router/app_router.dart';
 import 'package:flutter_template/ui/resources.dart';
 import 'package:flutter_template/ui/theme/app_theme.dart';
 
@@ -14,47 +10,24 @@ class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => BlocProvider(
-        create: (_) => MainCubit(),
-        child: _SplashContentScreen(),
-      );
-}
-
-class _SplashContentScreen extends StatelessWidget {
-  final router = DiProvider.get<AppRouter>();
-
-  @override
-  Widget build(BuildContext context) => BlocBuilder<MainCubit, MainBaseState>(
-        builder: (context, state) => MaterialApp.router(
-          theme: AppTheme.provideAppTheme(context),
-          routerDelegate: AutoRouterDelegate.declarative(
-            router,
-            routes: (_) => provideRoutes(state),
-          ),
-          routeInformationParser:
-              router.defaultRouteParser(includePrefixMatches: true),
-          localizationsDelegates: const [
-            AppLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          supportedLocales: AppLocalizations.supportedLocales,
-          builder: (context, child) {
-            Resources.setup(context);
-            return child!;
-          },
-        ),
-      );
-
-  List<PageRouteInfo<dynamic>> provideRoutes(MainBaseState state) {
-    switch (state.authenticationStatus) {
-      case AuthenticationStatus.authenticated:
-        return [const AuthenticatedRouter()];
-      case AuthenticationStatus.unauthenticated:
-        return [const UnauthenticatedRouter()];
-      case null:
-        return [];
-    }
+  Widget build(BuildContext context) {
+    final router = DiProvider.get<AppRouter>();
+    return MaterialApp.router(
+      theme: AppTheme.provideAppTheme(context),
+      routerDelegate: router.delegate(),
+      routeInformationParser:
+          router.defaultRouteParser(includePrefixMatches: true),
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: AppLocalizations.supportedLocales,
+      builder: (context, child) {
+        Resources.setup(context);
+        return child!;
+      },
+    );
   }
 }
