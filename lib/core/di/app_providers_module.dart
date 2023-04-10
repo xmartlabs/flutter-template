ff477ea3e147e9def29a1660d724cfe9827517af
Sol Mendiola <57235692+SolMendiola@users.noreply.github.com>
2023-04-10 14:36:04 -0300

    feat: improve shared preferences layer (#163)

--- lib/core/di/app_providers_module.dart
+++ lib/core/di/app_providers_module.dart
@@ -1,5 +1,8 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_template/core/source/common/app_database.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_template/core/source/common/local_shared_preferences_storage.dart';
 
 class AppProvidersModule {
   AppProvidersModule._privateConstructor();
@@ -19,6 +22,15 @@ extension _GetItDiModuleExtensions on GetIt {
   void _setupModule() {
     registerSingletonAsync(
       () => $FloorAppDatabase.databaseBuilder('app_database.db').build(),
+    );
+    registerLazySingleton(FlutterSecureStorage.new);
+    registerSingletonAsync(() => SharedPreferences.getInstance());
+
+    registerSingletonAsync(
+      () async => LocalSharedPreferencesStorage(
+        get(),
+        await getAsync<SharedPreferences>(),
+      ).init(),
     );
   }
 }
