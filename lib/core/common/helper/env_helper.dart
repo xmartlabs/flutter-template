b250389774bb96200f66d644ba997a8cc85cc39c
Gustavo Franco <gfran.93@gmail.com>
2023-06-09 12:57:29 -0300

    fix: improve code

--- lib/core/common/helper/env_helper.dart
+++ lib/core/common/helper/env_helper.dart
@@ -1,28 +1,13 @@
 import 'dart:async';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_template/core/common/logger.dart';
 
 Future<Map<String, String>> loadEnvs(
   String path, [
   bool ignoreErrors = true,
 ]) async {
-  final dotEnv = DotEnv();
   final completer = Completer<Map<String, String>>();
-  // ignore: unawaited_futures
-  runZonedGuarded(
-    () async {
-      await dotEnv.load(fileName: path, isOptional: true);
-      completer.complete(dotEnv.env);
-    },
-    (e, s) {
-      if (ignoreErrors) {
-        completer.complete({});
-      } else {
-        Logger.d('$path file is not valid.', e);
-        completer.completeError(e, s);
-      }
-    },
-  );
+  await dotenv.load(fileName: path, isOptional: true);
+  completer.complete(dotenv.env);
   return completer.future;
 }
