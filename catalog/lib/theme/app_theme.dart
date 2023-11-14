db5d04abc00c8666ec8f8aced25c32473aa653b5
Sol Mendiola <solmendiola73@gmail.com>
2023-11-14 12:02:11 -0300

    resolve comments

--- catalog/lib/theme/app_theme.dart
+++ catalog/lib/theme/app_theme.dart
@@ -2,6 +2,7 @@ import 'package:catalog/theme/app_text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:catalog/theme/app_colors.dart';
 import 'package:catalog/theme/app_dimensions.dart';
+//TODO: add theme extensions
 
 late AppColors _colors;
 late AppDimens _dimensions;
