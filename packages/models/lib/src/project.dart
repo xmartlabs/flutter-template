9e3b398a7eba78df1819c693e371b4c5826e150d
Federico Correa <federico.correa.lima@gmail.com>
2023-12-01 13:50:49 -0300

    refactor: move catalog to packages and create models package

---
+++ packages/models/lib/src/project.dart
@@ -0,0 +1,19 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'project.freezed.dart';
+part 'project.g.dart';
+
+@freezed
+class Project with _$Project {
+  factory Project({
+    required int id,
+    required String name,
+    required String description,
+    required String url,
+    required String imageUrl,
+    required String language,
+  }) = _Project;
+
+  factory Project.fromJson(Map<String, dynamic> json) =>
+      _$ProjectFromJson(json);
+}
---
+++ packages/models/lib/src/project.freezed.dart
@@ -0,0 +1,254 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'project.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+Project _$ProjectFromJson(Map<String, dynamic> json) {
+  return _Project.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Project {
+  int get id => throw _privateConstructorUsedError;
+  String get name => throw _privateConstructorUsedError;
+  String get description => throw _privateConstructorUsedError;
+  String get url => throw _privateConstructorUsedError;
+  String get imageUrl => throw _privateConstructorUsedError;
+  String get language => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $ProjectCopyWith<Project> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ProjectCopyWith<$Res> {
+  factory $ProjectCopyWith(Project value, $Res Function(Project) then) =
+      _$ProjectCopyWithImpl<$Res, Project>;
+  @useResult
+  $Res call(
+      {int id,
+      String name,
+      String description,
+      String url,
+      String imageUrl,
+      String language});
+}
+
+/// @nodoc
+class _$ProjectCopyWithImpl<$Res, $Val extends Project>
+    implements $ProjectCopyWith<$Res> {
+  _$ProjectCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+    Object? description = null,
+    Object? url = null,
+    Object? imageUrl = null,
+    Object? language = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      description: null == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      url: null == url
+          ? _value.url
+          : url // ignore: cast_nullable_to_non_nullable
+              as String,
+      imageUrl: null == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      language: null == language
+          ? _value.language
+          : language // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_ProjectCopyWith<$Res> implements $ProjectCopyWith<$Res> {
+  factory _$$_ProjectCopyWith(
+          _$_Project value, $Res Function(_$_Project) then) =
+      __$$_ProjectCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {int id,
+      String name,
+      String description,
+      String url,
+      String imageUrl,
+      String language});
+}
+
+/// @nodoc
+class __$$_ProjectCopyWithImpl<$Res>
+    extends _$ProjectCopyWithImpl<$Res, _$_Project>
+    implements _$$_ProjectCopyWith<$Res> {
+  __$$_ProjectCopyWithImpl(_$_Project _value, $Res Function(_$_Project) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+    Object? description = null,
+    Object? url = null,
+    Object? imageUrl = null,
+    Object? language = null,
+  }) {
+    return _then(_$_Project(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      description: null == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      url: null == url
+          ? _value.url
+          : url // ignore: cast_nullable_to_non_nullable
+              as String,
+      imageUrl: null == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      language: null == language
+          ? _value.language
+          : language // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_Project implements _Project {
+  _$_Project(
+      {required this.id,
+      required this.name,
+      required this.description,
+      required this.url,
+      required this.imageUrl,
+      required this.language});
+
+  factory _$_Project.fromJson(Map<String, dynamic> json) =>
+      _$$_ProjectFromJson(json);
+
+  @override
+  final int id;
+  @override
+  final String name;
+  @override
+  final String description;
+  @override
+  final String url;
+  @override
+  final String imageUrl;
+  @override
+  final String language;
+
+  @override
+  String toString() {
+    return 'Project(id: $id, name: $name, description: $description, url: $url, imageUrl: $imageUrl, language: $language)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_Project &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.description, description) ||
+                other.description == description) &&
+            (identical(other.url, url) || other.url == url) &&
+            (identical(other.imageUrl, imageUrl) ||
+                other.imageUrl == imageUrl) &&
+            (identical(other.language, language) ||
+                other.language == language));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode =>
+      Object.hash(runtimeType, id, name, description, url, imageUrl, language);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_ProjectCopyWith<_$_Project> get copyWith =>
+      __$$_ProjectCopyWithImpl<_$_Project>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_ProjectToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Project implements Project {
+  factory _Project(
+      {required final int id,
+      required final String name,
+      required final String description,
+      required final String url,
+      required final String imageUrl,
+      required final String language}) = _$_Project;
+
+  factory _Project.fromJson(Map<String, dynamic> json) = _$_Project.fromJson;
+
+  @override
+  int get id;
+  @override
+  String get name;
+  @override
+  String get description;
+  @override
+  String get url;
+  @override
+  String get imageUrl;
+  @override
+  String get language;
+  @override
+  @JsonKey(ignore: true)
+  _$$_ProjectCopyWith<_$_Project> get copyWith =>
+      throw _privateConstructorUsedError;
+}
