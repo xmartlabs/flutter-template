808a7ff2221b4ef72f24827d9dc745e82af13e94
Sol Mendiola <57235692+SolMendiola@users.noreply.github.com>
2023-12-06 17:00:44 -0300

    feat: add buttons (#195)

--- catalog/lib/theme/app_theme.dart
+++ catalog/lib/theme/app_theme.dart
@@ -1,3 +1,4 @@
+import 'package:catalog/theme/app_buttons.dart';
 import 'package:catalog/theme/app_text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:catalog/theme/app_colors.dart';
@@ -7,6 +8,7 @@ import 'package:catalog/theme/app_dimensions.dart';
 late AppColors _colors;
 late AppDimens _dimensions;
 late AppTextStyles _styles;
+late AppButtonsStyle _buttonStyles;
 
 class AppTheme {
   static ThemeData provideAppTheme(BuildContext buildContext) {
@@ -14,12 +16,28 @@ class AppTheme {
     _styles = AppTextStyles.getAppStyles();
     _dimensions = AppDimens.getDimensions();
     _colors = AppColors.getColorScheme();
+    _buttonStyles = AppButtonsStyle.getButtonTheme();
 
     return ThemeData(
       primaryColor: _colors.primary,
       colorScheme: _colors,
-      textTheme: _styles.getThemeData(),
-      primaryTextTheme: _styles.getThemeData(),
+      filledButtonTheme: FilledButtonThemeData(
+        style: _buttonStyles.filledButton,
+      ),
+      outlinedButtonTheme: OutlinedButtonThemeData(
+        style: _buttonStyles.outlineButton,
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: _buttonStyles.textButton,
+      ),
+      textTheme: _styles.getThemeData().apply(
+            bodyColor: _colors.textColor,
+            displayColor: _colors.textColor.shade500,
+          ),
+      appBarTheme: AppBarTheme(
+        backgroundColor: _colors.primary.shade400,
+        titleTextStyle: TextStyle(color: _colors.textColor.shade100),
+      ),
     );
   }
 }
@@ -30,4 +48,6 @@ extension ThemeExtensions on ThemeData {
   AppColors get colors => _colors;
 
   AppTextStyles get textStyles => _styles;
+
+  AppButtonsStyle get buttonsStyle => _buttonStyles;
 }
