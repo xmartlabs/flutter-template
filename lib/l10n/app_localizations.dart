4132ccef779afb6aa19b47710551c96469d22927
Nicolás Lantean <nlantean@hotmail.com>
2025-07-24 13:55:24 -0300

    fix: format

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -192,8 +192,9 @@ AppLocalizations lookupAppLocalizations(Locale locale) {
   }
 
   throw FlutterError(
-      'AppLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
-      'an issue with the localizations generation tool. Please file an issue '
-      'on GitHub with a reproducible sample app and the gen-l10n configuration '
-      'that was used.');
+    'AppLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
+    'an issue with the localizations generation tool. Please file an issue '
+    'on GitHub with a reproducible sample app and the gen-l10n configuration '
+    'that was used.',
+  );
 }
